96b07e120bda3ffb97131ddd77e17669a9dab929
momnica <55236837+momnica@users.noreply.github.com>
2025-07-10 16:53:36 +0000

    fix: set base path for main

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/qmate-demo/',  // 👈 questo è il nome del repo su GitHub
+  base: '/',  // 👈 questo è il nome del repo su GitHub
   plugins: [react()],
 });
